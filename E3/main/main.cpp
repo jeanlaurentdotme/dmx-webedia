e9db919666ddf9f0458c3cc033f7f64d4ed743b0
duvalwilliam <91595172+duvalwilliam@users.noreply.github.com>
2023-01-11 17:38:47 +0100

    Add files via upload

---
+++ E3/main/main.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <iostream>
+#include <stdio.h>
+
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+#include <stdlib.h>
+
+#include "point.h"
+using namespace std;
+
+const string server = "tcp://141.94.61.8:3306";
+const string username = "bdd@141.94.61.8";
+const string password = "bdd";
+
+int main(int argc, char ** argv)
+{
+	sql::Driver *driver;
+	sql::Connection *con;
+	sql::Statement *stmt;
+	sql::PreparedStatement *pstmt;
+
+	try
+	{
+		driver = get_driver_instance();
+		con = driver->connect(server, username, password);
+	}
+	catch (sql::SQLException e)
+	{
+		cout << "Could not connect to server. Error message: " << e.what() << endl;
+		system("pause");
+		exit(1);
+	}
+
+	//please create database "quickstartdb" ahead of time
+	con->setSchema("quickstartdb");
+
+	stmt = con->createStatement();
+	stmt->execute("DROP TABLE IF EXISTS inventory");
+	cout << "Finished dropping table (if existed)" << endl;
+	stmt->execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);");
+	cout << "Finished creating table" << endl;
+	delete stmt;
+
+	pstmt = con->prepareStatement("INSERT INTO inventory(name, quantity) VALUES(?,?)");
+	pstmt->setString(1, "banana");
+	pstmt->setInt(2, 150);
+	pstmt->execute();
+	cout << "One row inserted." << endl;
+
+	pstmt->setString(1, "orange");
+	pstmt->setInt(2, 154);
+	pstmt->execute();
+	cout << "One row inserted." << endl;
+
+	pstmt->setString(1, "apple");
+	pstmt->setInt(2, 100);
+	pstmt->execute();
+	cout << "One row inserted." << endl;
+
+	delete pstmt;
+	delete con;
+	system("pause");
+	return 0;
+}
---
+++ E3/main/point.h
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+
+class Scene {
+public:
+	void createScene(int id, std::string name);
+	void updateScene(int id, std::string name);
+	void deleteScene(int id);
+
+	std::string getSceneName(int id);
+	std::vector<int> getAllSceneIds();
+};
+
+class Lighting {
+public:
+	void setLightIntensity(int id, float intensity);
+	void setLightColor(int id, std::string color);
+
+	float getLightIntensity(int id);
+	std::string getLightColor(int id);
+};
+
+class Database {
+public:
+	void insertScene(int id, std::string name);
+	void updateScene(int id, std::string name);
+	void deleteScene(int id);
+
+	std::string getSceneName(int id);
+	std::vector<int> getAllSceneIds();
+
+	void insertLight(int id, std::string name, float intensity, std::string color);
+	void updateLight(int id, std::string name, float intensity, std::string color);
+	void deleteLight(int id);
+
+	std::string getLightName(int id);
+	float getLightIntensity(int id);
+	std::string getLightColor(int id);
+};
+
