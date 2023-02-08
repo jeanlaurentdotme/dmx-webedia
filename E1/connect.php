195dcac5d7133972af2b31abf7c5172e4e5dbe95
Chaokobo <91595312+Chaokobo@users.noreply.github.com>
2023-02-08 17:03:59 +0100

    Add files via upload

---
+++ E1/connect.php
@@ -0,0 +1,6 @@
+<?php
+define ('SERVEUR', "localhost:3306");
+define ('LOGIN', "bdd");
+define ('MDP', "bdd");
+define ('BDD', "PROJETWEBEDIA");
+?>
---
+++ E1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Gestion de scènes</title>
+    <link rel='stylesheet' href='css/main.css'/>
+  <!---
+    <script src='main.js'></script>
+  -->
+  <link rel="icon" type="image/png" href="images/icone.png"/>
+</head>
+<body>
+    <a href="accueil.php">Espace membre</a>
+</body>
+</html>
