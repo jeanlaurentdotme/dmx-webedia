a342ff2e3a6218764c68e0d987772c2b42410126
Chaokobo <91595312+Chaokobo@users.noreply.github.com>
2023-03-14 17:43:19 +0100

    Add files via upload

---
+++ E1/code/inscription.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Inscription</title>
+    <link rel='stylesheet' href='css/main.css'/>
+  <!---
+    <script src='main.js'></script>
+  -->
+  <link rel="icon" type="image/png" href="images/icone.png"/>
+</head>
+<body>
+    Inscription à l'espace membre :<br/>
+    <form action="verification.php" method="post">
+        pseudo<input type="text" name="nom" value=""><br />
+        login<input type="text" name="login" value=""><br />
+        mot de passe<input type="password" name="mdp1" value=""><br />
+        confirmation mdp<input type="password" name="mdp2" value=""><br />
+        <input type="submit" name="inscription" value="Inscription">
+    </form>
+</body>
+</html>
