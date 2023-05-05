486970971a30c7b45f71f7a7469442df36ac7aa8
ValentinBorgne <73233897+ValentinBorgne@users.noreply.github.com>
2023-05-05 14:36:14 +0200

    Add files via upload

--- E2-BORGNE_Valentin/Projet_2/Projet_2.ino
+++ E2-BORGNE_Valentin/Projet_2/Projet_2.ino
@@ -4,40 +4,54 @@ String val;
 long nbValues;
 long cumulP1;
 bool sendValuePot = false;
+bool sendText = false;
 String sentence2, sentence;
+int bp1 = 12;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(22, INPUT_PULLUP);
+  pinMode(bp1, INPUT_PULLUP);
   lcd.begin(16, 2);
+  Serial.setTimeout(10);
 }
 
 void loop() {
+  int b1 = digitalRead(bp1);
   if (nbValues == 100)
   {
    cumulP1 = 0;
    nbValues = 0;
   }
-  cumulP1 += analogRead(A0);
+  cumulP1 += analogRead(A0)/4;
   nbValues++;
   if (Serial.available() > 0)
   {
-    //lcd.clear();
     val = Serial.readString();
-    //lcd.setCursor(0, 0);
-    //lcd.print(val);
     if (val=="1")
-    {
-      sendValuePot = true;
-    }
-    if (sendValuePot)
     {
         sentence = "{\"p1\":";
         sentence += cumulP1 / nbValues;
         sentence += "}";
         Serial.println(sentence);
-        sendValuePot = false;
+      //sendValuePot = true;
     }
-    delay(10);
+    else
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      //lcd.print(val);
+      lcd.print("tu joue a cs");
+      lcd.setCursor(0,1);
+      lcd.print("apres genshin");
+    }
+    /*if (sendValuePot)
+    {
+        sendValuePot = false;
+    }*/
   }
+    if (b1 == LOW)
+    {
+      Serial.println("button pressed");
+      delay(100);
+    }
 }
