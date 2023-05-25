22323d28cd79b181eff49257293c70e6d60e92ae
Chaokobo <91595312+Chaokobo@users.noreply.github.com>
2023-05-25 15:23:37 +0200

    Add files via upload

---
+++ E1/25_05_23/connect.php
@@ -0,0 +1,7 @@
+<?php
+// définie des noms pour les différents identifiants de connexion afin de ne pas les insérer dans le code
+define ('SERVEUR', '192.168.64.144');
+define ('LOGIN', "root");
+define ('MDP', "root");
+define ('BDD', "PROJETWEBEDIA");
+?>
